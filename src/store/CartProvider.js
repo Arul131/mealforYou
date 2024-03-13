8c38153cde173b27c9df9b26b3ba6c3ccc018cd4
arul_chambyal <arul.chambyal@antiersolutions.com>
2024-03-13 15:47:32 +0530

    latest changes and optimizing cart provider component

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,6 +1,6 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
-import CartContext from './cart-context';
+import CartContext from "./cart-context";
 
 const defaultCartState = {
   items: [],
@@ -8,54 +8,59 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === 'ADD') {
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
+  switch (action.type) {
+    case "ADD":
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+      const existingCartItem = state.items[existingCartItemIndex];
+      let updatedItems;
 
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
-    );
-    const existingCartItem = state.items[existingCartItemIndex];
-    let updatedItems;
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems = [...state.items, action.item];
+      }
 
-    if (existingCartItem) {
-      const updatedItem = {
-        ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount,
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
       };
-      updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
-    } else {
-      updatedItems = state.items.concat(action.item);
-    }
 
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount,
-    };
+    case "REMOVE":
+      const existingItemIndex = state.items.findIndex(
+        (item) => item.id === action.id
+      );
+      const existingItem = state.items[existingItemIndex];
+      let updatedCartItems;
+      const updatedTotalAmt = state.totalAmount - existingItem.price;
+
+      if (existingItem.amount === 1) {
+        updatedCartItems = state.items.filter((item) => item.id !== action.id);
+      } else {
+        const updatedItem = {
+          ...existingItem,
+          amount: existingItem.amount - 1,
+        };
+        updatedCartItems = [...state.items];
+        updatedCartItems[existingItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedCartItems,
+        totalAmount: updatedTotalAmt,
+      };
+
+    default:
+      return defaultCartState;
   }
-  if (action.type === 'REMOVE') {
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.id
-    );
-    const existingItem = state.items[existingCartItemIndex];
-    const updatedTotalAmount = state.totalAmount - existingItem.price;
-    let updatedItems;
-    if (existingItem.amount === 1) {
-      updatedItems = state.items.filter(item => item.id !== action.id);
-    } else {
-      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
-      updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
-    }
-
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount
-    };
-  }
-
-  return defaultCartState;
 };
 
 const CartProvider = (props) => {
@@ -65,11 +70,11 @@ const CartProvider = (props) => {
   );
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({ type: 'ADD', item: item });
+    dispatchCartAction({ type: "ADD", item: item });
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: 'REMOVE', id: id });
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   const cartContext = {
